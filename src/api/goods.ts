c2b08f1745de246b8302087d39e7027da0fdf599
Yana Ovcharyk <yanaovcharik@gmail.com>
2025-08-21 21:26:19 +0300

    Add solution2

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,16 +3,35 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
-export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+export async function getAll(): Promise<Good[]> {
+  try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to load goods. Status: ${response.status} ${response.statusText}`,
+      );
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    throw new Error(
+      'Failed to load goods: ' +
+        (err instanceof Error ? err.message : String(err)),
+    );
+  }
 }
 
-export const get5First = () => {
-  return getAll().then(goods =>
-    [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-  );
-};
+export async function get5First(): Promise<Good[]> {
+  const goods = await getAll();
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods.filter(good => good.color === 'red'));
-};
+  return [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+}
+
+export async function getRedGoods(): Promise<Good[]> {
+  const goods = await getAll();
+
+  return goods.filter(good => good.color === 'red');
+}
